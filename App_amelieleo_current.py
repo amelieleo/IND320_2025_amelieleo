3d45bbaab66f27254a4cdac063ad6a5d9cc9689d
amelieleo <aleonardi@uni-osnabrueck.de>
2025-10-23 20:30:24 +0200

    small changes to the app

--- App_amelieleo_current.py
+++ App_amelieleo_current.py
@@ -356,7 +356,7 @@ elif options == 'Energy Production Data': #----------------------ENERGY PRODUCTI
         #filtering the data for the selected price area
         filtered_data_area = production_data[production_data['pricearea'] == price_area]
         st.pyplot(create_pie_chart(filtered_data_area, price_area))
-        st.info("'other' groups include all categories below 2% of total (e.g. thermal, wind, solar, other).", icon="ℹ️")
+        st.info("Production Distribution: 'other' groups include all categories below 2% of total (e.g. thermal, wind, solar, other).", icon="ℹ️")
     
     with col2: #------------------Production groups and line plot------------------------
         options = ["hydro", "solar", "thermal", "wind", "other"]
@@ -370,7 +370,7 @@ elif options == 'Energy Production Data': #----------------------ENERGY PRODUCTI
             (production_data['productiongroup'].isin(production_groups)) &
             (production_data['pricearea'] == price_area)
         ]
-        st.pyplot(create_lineplot_production(filtered_data_groups, price_area))
+        st.pyplot(create_lineplot_production(filtered_data_groups, price_area), width = "stretch")
         
 
     #source expander
--- Assignment2_App_amelieleo.py
+++ Assignment2_App_amelieleo.py
@@ -356,7 +356,7 @@ elif options == 'Energy Production Data': #----------------------ENERGY PRODUCTI
         #filtering the data for the selected price area
         filtered_data_area = production_data[production_data['pricearea'] == price_area]
         st.pyplot(create_pie_chart(filtered_data_area, price_area))
-        st.info("'other' groups include all categories below 2% of total (e.g. thermal, wind, solar, other).", icon="ℹ️")
+        st.info("Production Distribution: 'other' include all categories below 2% of total (e.g. thermal, wind, solar, other).", icon="ℹ️")
     
     with col2: #------------------Production groups and line plot------------------------
         options = ["hydro", "solar", "thermal", "wind", "other"]
