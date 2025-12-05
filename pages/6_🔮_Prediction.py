f5b96891e02e8b5e2523d01b3b1b9e716a838b9d
amelieleo <aleonardi@uni-osnabrueck.de>
2025-12-05 11:55:25 +0100

    bug fix

--- pages/6_🔮_Prediction.py
+++ pages/6_🔮_Prediction.py
@@ -53,7 +53,7 @@ if train_end < train_start:
 
 start_ts = pd.to_datetime(train_start).tz_localize(TIMEZONE)#.tz_convert("UTC")
 
-end_ts = (pd.to_datetime(train_end) + pd.Timedelta(days=1))#.tz_localize(TIMEZONE).tz_convert("UTC")  # make end exclusive
+end_ts = (pd.to_datetime(train_end) + pd.Timedelta(days=1)).tz_localize(TIMEZONE)#.tz_convert("UTC")  # make end exclusive
 steps = int(horizon_days) * 24     
 
 
